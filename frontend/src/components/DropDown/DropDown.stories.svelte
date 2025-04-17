0783170649695e4d4ebe9aae7f72c356d89bf769
unknown <conghuy649@gmail.com>
2025-04-17 17:08:03 +0700

    messing around with storybook

---
+++ frontend/src/components/DropDown/DropDown.stories.svelte
@@ -0,0 +1,14 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import DropDown from './DropDown.svelte';
+
+
+    const { Story } = defineMeta({
+        
+        component: DropDown,
+
+    });
+
+</script>
+
+<Story name="Default"/>
